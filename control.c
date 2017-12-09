adc5fb945d201ac04a0926d0757af94c16c70877
yedohkang913 <ykang@stuy.edu>
2017-12-09 10:23:57 -0500

    finished control.c

--- control.c
+++ control.c
@@ -5,64 +5,101 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <string.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <errno.h>
 
+void print_file(char *file) {
+	FILE *f = fopen(file, "r");
+	if (!f) {
+		return;
+	}
+	char input[256];
+	while (fgets(input, sizeof(input), f)) {
+		printf("%s", input);
+	}
+	fclose(f);
+	printf("\n");
+}
 
 int main(int argc, char* argv[]) {
 	
 	umask(0000);
 	
-	int num = 0;
-	int ds = 0;
-	
-	int KEY = 12345;
+	int semKEY = 12345;
+	int shmKEY = 13579;
 
 	if (argv[1] == NULL) {
-		printf("Please enter in an argument.");
+		printf("Please enter in an argument.\n");
 		exit(1);
+	}
+	
+	int semID = semget(semKEY, 1, IPC_CREAT | 0644);
+	if (semID == -1) {
+		printf("Error: %s\n", strerror(errno));
+		return 1;
 	}
 	
 	if (strcmp(argv[1], "-c") == 0) {
 		//Should make the shared memory, semaphore and file (open the file with the truncate flag). 
 		//Set any values that are needed.
-		int fd = open("story.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		int sm = shmget(KEY, sizeof(int), IPC_CREAT | IPC_EXCL);
-		ds = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0600);
-		if (ds == -1) {
-			exit(1);
+		int shm = shmget(shmKEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
+		if (shm == -1) {
+			printf("Error: %s\n", strerror(errno));
+			return 1;
 		}
-		//num = atoi(argv[2]);
-		/*
-		if(!argv[2]){
-			printf("-c requires a second command (integer)!\n");
-			exit(0);
+		
+		int fd = open("story.txt", O_CREAT | O_TRUNC, 0644);
+		if (fd == -1) {
+			printf("Error: %s\n", strerror(errno));
+			return 1;
 		}
-		*/
-		if (ds == -1) {
-			printf("semaphore already exists\n");
-			exit(1);
+		
+		union semun value;
+		value.val = 1;
+
+		semID = semctl(semID, 0, SETVAL, value);
+		if (semID == -1) {
+			printf("Error: %s\n", strerror(errno));
+			return 1;
 		}
-		else {
-			printf("semaphore created: %d\n", ds);
-			//printf("value set: %d\n", semctl(ds, 0, GETVAL));
-			semctl(ds, 0, SETVAL, 1);
-			
-		}
+
+		close(fd);
 		printf("story.txt created!\n");
 	}
 	
 	if (strcmp(argv[1], "-v") == 0) {
-		//output contents of the story file
-	  int fd1 = open("story.txt", O_RONLY, 0644);
+		// Output the contents of the story file. 
+		print_file("story.txt");
 	}
 	
 	if (strcmp(argv[1], "-r") == 0) {
 		//Remove the shared memory and the semaphore
 		//Display the full contents of the story.
-		ds = semget(KEY, 1, 0600);
-		printf("semaphore removed: %d\n", semctl(ds, 0, IPC_RMID));
-		fd = shmctl(ds, IPC_RMID, NULL);
-		printf("shared memory deleted: %d\n", fd);
-		int fd2 = open("story.txt", O_RONLY, 0644);
+		
+		semID = semget(semKEY, 1, 0600);
+		semID = semctl(semID, 0, IPC_RMID);
+		if (semID == -1) {
+			printf("Error: %s\n", strerror(errno));
+			return 1;
+		}
+
+		int shm = shmget(shmKEY, sizeof(int), 0);
+		if (shm == -1) {
+			printf("Error: %s\n", strerror(errno));
+			return 1;
+		}
+		
+		struct shmid_ds buf;
+		shm = shmctl(shm, IPC_RMID, &buf);
+		if (shm == -1) {
+			printf("Error: %s\n", strerror(errno));
+			return 1;
+		}
+		
+		printf("shared memory deleted: %d\n", shm);
+		printf("semaphone deleted: %d\n", semID);
+		print_file("story.txt");
 		
 	}
 	
